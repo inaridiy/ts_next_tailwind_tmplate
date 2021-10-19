7b9591d7e49ac20e38827ce028b5b9888b754141
inaridiy <inainainariinainari@gmail.com>
2021-10-19 13:01:13 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className='text-6xl font-bold'>Hello Next,Typescript,Eslint,Prettier and TailwindCSS</div>
+  );
+};
+
+export default Home;
